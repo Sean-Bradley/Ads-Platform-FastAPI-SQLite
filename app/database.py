fae57a070ef26b32b8cfa849bb14a565eeb8febb
Sean Bradley <seanwasere@gmail.com>
2026-05-24 11:11:50 +0100

    Migrate application database from SQLite to PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,20 +1,37 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.config import DATABASE_PATH
-
-DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+from app.config import (
+    DATABASE_TYPE,
+    DATABASE_HOST,
+    DATABASE_PORT,
+    DATABASE_NAME,
+    DATABASE_USER,
+    DATABASE_PASSWORD,
+    DATABASE_PATH,
 )
+
+if DATABASE_TYPE == "postgresql":
+    DATABASE_URL = (
+        f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}"
+        f"@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    )
+
+    engine = create_engine(DATABASE_URL)
+
+else:
+    DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
+
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False},
+    )
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
 
 Base = declarative_base()
