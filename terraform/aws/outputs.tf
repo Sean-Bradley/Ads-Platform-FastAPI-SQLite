816e73744a65d357116e4141df29c318cc0ce0de
Sean Bradley <seanwasere@gmail.com>
2026-05-27 12:54:47 +0100

    moved iam related outputs to grafana-iam.tf and del;eted outputs.tf

--- terraform/aws/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "grafana_access_key_id" {
-  value = aws_iam_access_key.grafana.id
-}
-
-output "grafana_secret_access_key" {
-  value     = aws_iam_access_key.grafana.secret
-  sensitive = true
-}
